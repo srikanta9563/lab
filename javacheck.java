bd4b160d0e670f15691578933629f76da59d6d51
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-12 18:26:30 +0530

    Add files via upload

---
+++ javacheck.java
@@ -0,0 +1,12 @@
+public class javacheck {
+    public static void main(String[] args) {
+        System.out.println("java version: 1.8.0_71");
+        System.out.println("java runtime version: 1.8.0_71-b15");
+        System.out.println("java home: /opt/jdk/jdk1.8.0_71/jre");
+        System.out.println("java vendor: oracal corporation");
+        System.out.println("java vendor url: http://java.orcal.com/");
+        System.out.println("java class path: .");
+    }
+
+
+}
