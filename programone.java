2f0868e22e4f3d708f126135425dc546be23ac96
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-18 23:07:52 +0530

    Add files via upload

---
+++ programone.java
@@ -0,0 +1,18 @@
+// 1 write a program nto find the value of specified expression
+public class programone {
+    public static void main(String[] args) {
+        int a =(101+0)/3;
+        double b =(3.0e-6*10000000.1);
+        boolean c =(true&& true);
+        boolean d =(false && true);
+        boolean e =(false && false)||(true&&true);
+        boolean f =(false || false)&&(true&&true);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+    }
+}
