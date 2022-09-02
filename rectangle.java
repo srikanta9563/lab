23d5758f27884ac3696a6d86c2bdc6dfc23839c8
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-09-02 18:39:58 +0530

    Update rectangle.java

--- rectangle.java
+++ rectangle.java
@@ -1,3 +1,13 @@
+/*2 you will make a class ‘Rectangle’ and declare the variable length and breadth as private.
+One Constructor will be used for initializing the value of variables. When an object is created,
+the constructor will be called and the default value of the instance variables will be assigned.
+Now create another new class ‘RectangleTest’ & create an object of class Rectangle and
+assign values of the parameter used in constructor. Calculate area of the rectangle and print
+it on the console. Let’s update the new value of variable. And then read the updated value.*/
+
+
+
+
 import java.util.Scanner;
 public class rectangle {
     private int length = 0;
