7b87378c1a25eb954cdd9599a9fb8c45cef36a29
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-09-09 16:24:43 +0530

    Add files via upload

---
+++ employee1.java
@@ -0,0 +1,18 @@
+ public class employee1 {
+    employee1(String name, int id){
+        System.out.println("the name of the employee is" +name+"and id is"+id);
+
+
+    }
+}
+ public class employee {
+     public static void main(String[] args) {
+         employee1 obj1 = new employee1("srikanta", 10);
+         employee1 obj2 = new employee1("sandip", 11);
+         employee1 obj3 = new employee1("sourav", 12);
+         employee1 obj4 = new employee1("sandip", 13);
+         employee1 obj5 = new employee1("sumit", 14);
+     }
+ }
+
+
