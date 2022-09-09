82b9f214b28acd9d3f71aa5a106eac1fb373938a
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-09-09 11:22:17 +0530

    Add files via upload

---
+++ question2.java
@@ -0,0 +1,13 @@
+
+//Develop a public java class and make sure nobody can create any object of the class from outside the class
+
+
+public class question2 {
+    private question2(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
