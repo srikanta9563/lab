e91427542d72fb24afb1658301eed7e7c5a110c6
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-10-12 14:26:29 +0530

    Update oddindexcheck.java

--- lab/oddindexcheck.java
+++ lab/oddindexcheck.java
@@ -1,3 +1,4 @@
+//we need to print the elements of the array which are present array in odd position
 public class oddindexcheck {
     public static void main(String[] args) {
         String a = "ABCDEFG";
