d225d7b4202f3ffd06895e386f6278b563773147
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-10-14 18:29:37 +0530

    Update sequencestring.java

--- 13_oct lab/sequencestring.java
+++ 13_oct lab/sequencestring.java
@@ -1,14 +1,14 @@
 //Write a Java program to find sequences of lowercase letters joined with a underscore
 
 
-public class sequencestring {
+public class sequencestring { //main class
     public static void main(String[] args) {
 
             System.out.println(validate("srikanta_biswas"));
             System.out.println(validate("SRI_kanta"));
         }
         public static String validate(String text) {
-            if (text.matches("^[a-z]+_[a-z]+"))
+            if (text.matches("^[a-z]+_[a-z]+")) // creating the logic
                 return " match!";
             else
                 return "Not matched!";
